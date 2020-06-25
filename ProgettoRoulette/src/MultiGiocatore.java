f0402569891cfbc1a8009e601486cd3a3492cef4
Chiara <stroppolo_chiara@yahoo.it>
2020-06-25 20:17:41 +0200

    inserita possibilità di avere più client

---
+++ ProgettoRoulette/src/MultiGiocatore.java
@@ -0,0 +1,22 @@
+
+public class MultiGiocatore {
+	
+	public MultiGiocatore(){
+	}
+	void myRun(){
+		new Giocatore(0);
+		new Giocatore(1);
+		new Giocatore(2);
+		new Giocatore(3);
+		new Giocatore(4);
+		new Giocatore(5);
+		new Giocatore(6);
+		new Giocatore(7);
+		new Giocatore(8);
+		new Giocatore(9);
+		
+	}
+	public static void main(String[] args) {
+		new MultiGiocatore().myRun();
+	}
+}
