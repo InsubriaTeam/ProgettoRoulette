b52e94a0934063e0523139f1448ec988c674100c
Chiara <stroppolo_chiara@yahoo.it>
2020-06-26 00:49:50 +0200

    2

--- ProgettoRoulette/src/RouletteInterface2.java
+++
@@ -1,8 +0,0 @@
-import java.rmi.Remote;
-import java.rmi.RemoteException;
-
-public interface RouletteInterface2 extends Remote{
-	
-	public void addGiocatore(Giocatore2 g) throws RemoteException;
-	public void removeGiocatore(Giocatore2 g) throws RemoteException;
-}
