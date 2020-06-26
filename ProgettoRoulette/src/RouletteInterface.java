fddd797280d3d827b6fd98a794384a7ebd6f2930
Chiara <stroppolo_chiara@yahoo.it>
2020-06-26 23:37:28 +0200

    tolto 1 metodo

--- ProgettoRoulette/src/RouletteInterface.java
+++ ProgettoRoulette/src/RouletteInterface.java
@@ -6,6 +6,5 @@ import java.util.ArrayList;
 public interface RouletteInterface extends Remote {
 	public void game(int id, ArrayList<Double> allQuote, ArrayList<Integer> allNum, int play, String showmustgoon) throws RemoteException; 
 	public int/*o boolean a seconda del codice*/ busy() throws RemoteException; //TODO controller max persone
-	public boolean ext() throws RemoteException; //TODO controller estrazione
 	public int winner() throws RemoteException;//TODO 
 }
