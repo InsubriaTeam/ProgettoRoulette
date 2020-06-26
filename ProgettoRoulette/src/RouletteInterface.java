a3bbe69b5356de96b0329e8b533c89976dfa5d39
Chiara <stroppolo_chiara@yahoo.it>
2020-06-26 23:26:43 +0200

    pulizia, SISTEMATO INTERFACE

--- ProgettoRoulette/src/RouletteInterface.java
+++ ProgettoRoulette/src/RouletteInterface.java
@@ -4,12 +4,8 @@ import java.util.ArrayList;
 
 
 public interface RouletteInterface extends Remote {
-	
-	public void game(int id, ArrayList<Double> allQuote, ArrayList<Integer> allNum, int play, String showmustgoon ) throws RemoteException; 
-	public boolean busy(ArrayList<Giocatore2> players) throws RemoteException; 
-	public boolean ext() throws RemoteException; //controller estrazione
-	public int winner() throws RemoteException;
-	public void addPlayer(Giocatore2 g) throws RemoteException;
-	public void kickOut(Giocatore2 g) throws RemoteException;
-	
+	public void game(int id, ArrayList<Double> allQuote, ArrayList<Integer> allNum, int play, String showmustgoon) throws RemoteException; 
+	public int/*o boolean a seconda del codice*/ busy() throws RemoteException; //TODO controller max persone
+	public boolean ext() throws RemoteException; //TODO controller estrazione
+	public int winner() throws RemoteException;//TODO 
 }
