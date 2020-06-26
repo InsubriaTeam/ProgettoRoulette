d3a66842e09ecc897bce5d968205a6a006d14d70
Samu <Samu@SAM-PC.station>
2020-06-26 11:03:49 +0200

    unito roulette2 e partita1

--- ProgettoRoulette/src/RouletteInterface.java
+++ ProgettoRoulette/src/RouletteInterface.java
@@ -4,8 +4,10 @@ import java.util.ArrayList;
 
 
 public interface RouletteInterface extends Remote {
+	
 	public void game(int id, ArrayList<Double> allQuote, ArrayList<Integer> allNum, int play) throws RemoteException; 
-	public int/*o boolean a seconda del codice*/ busy() throws RemoteException; //TODO controller max persone
-	public boolean ext() throws RemoteException; //TODO controller estrazione
+	public boolean busy(ArrayList<Giocatore2> players) throws RemoteException; 
+	public boolean ext() throws RemoteException; //controller estrazione
 	public int winner() throws RemoteException;
+	
 }
